1d020a87ad89a2fd48c77a3e505fc0bc2ee2cf49
Lutuk145 <lukas.knej@gmail.com>
2023-07-29 19:44:46 +0200

    Added am=n error message when executing /destinations outside of a lootrun.

--- src/main/java/me/kmaxi/lootrunhelper/commands/ListBeaconDestinations.java
+++ src/main/java/me/kmaxi/lootrunhelper/commands/ListBeaconDestinations.java
@@ -1,21 +1,30 @@
 package me.kmaxi.lootrunhelper.commands;
 
 import com.mojang.brigadier.context.CommandContext;
+import me.kmaxi.lootrunhelper.beacon.BeaconChecker;
 import me.kmaxi.lootrunhelper.beacon.BeaconDestinations;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
+import static me.kmaxi.lootrunhelper.utils.CodingUtils.msg;
+
 public class ListBeaconDestinations {
 
     public static int run(CommandContext<FabricClientCommandSource> fabricClientCommandSourceCommandContext) {
-        System.out.println("Executing list command");
+        if (BeaconChecker.getLastBeacons() == null){
+            msg("§4No current beacons active.");
+            return 0;
+        }else {
 
-        BeaconDestinations.updateDestinations();
 
-        MinecraftClient.getInstance().player.sendMessage(Text.of(BeaconDestinations.destinations), false);
+            System.out.println("Executing list command");
+            BeaconDestinations.updateDestinations();
 
-        return 1;
+            MinecraftClient.getInstance().player.sendMessage(Text.of(BeaconDestinations.destinations), false);
+
+            return 1;
+        }
     }
 
 
